fd8b9f06b1b42a584d0cef89c24aba47144e9425
lilifuna <mujinn@interia.pl>
2018-01-10 01:40:13 +0100

    Initial

---
+++ apteka/App.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace apteka
+{
+
+    public static class App
+    {
+        public static Boolean initialized = false;
+
+        public static List<String> cart = new List<String>();
+
+
+        public static Hashtable products = new Hashtable()
+        {
+            {"Acodin",10 },
+            {"Thiocodin", 9 },
+            {"Tussidex",8 },
+            {"Dexacaps", 25},
+            {"Antidol", 20},
+            {"Mst 200", 15},
+            {"Fentanyl Actavis", 10},
+            {"OxyContin", 8},
+            {"Morfini Sulfas", 150},
+            {"Ibuprom", 5},
+            {"Diazepam", 25},
+            {"Karfentanyl", 100},
+            {"Lidokaina", 50},
+            {"Chloroform", 34},
+            {"Eter", 5},
+        };
+
+        
+
+    }
+}
--- apteka/Default.aspx.cs
+++ apteka/Default.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,68 @@ namespace apteka
 {
     public partial class _Default : Page
     {
+
+        List<CheckBoxList> categoriesLists = new List<CheckBoxList>();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            categoriesLists.Add(CheckBoxList1);
+            categoriesLists.Add(CheckBoxList2);
+            categoriesLists.Add(CheckBoxList3);
+            PopulateLists();
+        }
 
+        protected void productsCheckList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void categoriesRadioBtnList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int value = 0;
+            value = Int32.Parse(categoriesRadioBtnList.SelectedValue);
+            
+            foreach(CheckBoxList list in categoriesLists)
+            {
+                list.Visible = false;
+            }
+
+            categoriesLists[value - 1].Visible = true;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            foreach(CheckBoxList list in categoriesLists)
+            {
+                foreach(ListItem item in list.Items)
+                {
+                    if (item.Selected)
+                    {
+                        App.cart.Add(item.Value);
+                    }
+                }
+            }
+        }
+
+        private void PopulateLists()
+        {
+            if (App.initialized) return;
+            String[] items = new String[App.products.Count];
+            App.products.Keys.CopyTo(items, 0);
+            int k = 0;
+            int i = 1;
+            foreach(String item in items)
+            {
+                if(i == 5 && k < 2)
+                {
+                    k++;
+                    i = 1;
+                }
+                categoriesLists[k].Items.Add(item);
+                i++;
+            }
+
+            App.initialized = true;
         }
     }
 }
